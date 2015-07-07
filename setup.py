4faf93cd61ea9fd30a5f4c87de7c98096806e55c
Kyle Anderson <kwa@yelp.com>
2015-07-07 16:40:34 -0700

    Version bump to 0.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pysensu-yelp',
-    version='0.1.10',
+    version='0.2.0',
     provides=['pysensu_yelp'],
     description='Emits Yelp-flavored Sensu events to a Sensu Client',
     url='https://gitweb.yelpcorp.com/?p=pysensu-yelp.git',
