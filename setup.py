4fc6d5bfd7a872dc758841f6add1aa60a9959b82
Sheng Ran <ran@yelp.com>
2020-02-27 07:38:29 -0800

    Release 0.4.3
    - The previous version was incorrectly tagged

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pysensu-yelp',
-    version='0.4.2',
+    version='0.4.3',
     provides=['pysensu_yelp'],
     description='Emits Yelp-flavored Sensu events to a Sensu Client',
     url='https://github.com/Yelp/pysensu-yelp',
