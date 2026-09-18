4cade94534a7c23303e1de594c2b0f836f25ff06
milki (jrchu) <milki@yelp.com>
2026-09-18 10:53:39 -0700

    ^ F   Add support for py310, py311, py312

--- setup.py
+++ setup.py
@@ -14,6 +14,9 @@ setup(
     classifiers=[
          'Programming Language :: Python :: 3',
          'Programming Language :: Python :: 3.8',
+         'Programming Language :: Python :: 3.10',
+         'Programming Language :: Python :: 3.11',
+         'Programming Language :: Python :: 3.12',
     ],
     python_requires='>=3.8',
     package_data={
